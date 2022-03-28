305d02dcfa5e28abff05462cbc66db0008f8189d
rosemutai <chepngetichrose2030@gmail.com>
2022-03-28 07:42:38 +0000

    putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,16 @@
  *   * @c: The character to print
  *    *  
  *    */
-int _putchar(char c)
+int main(void)
 {
-return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return 0;
 }
