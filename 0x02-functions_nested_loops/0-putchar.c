40b8757ec5288fbed725ba5dc0186e08271bc5eb
rosemutai <chepngetichrose2030@gmail.com>
2022-03-28 07:54:02 +0000

    putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
 #include <main.h>
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *  
- *    */
+ * _putchar - writes the character c to stdout
+*/
+
+
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *sh = ('_putchar');
+	while(*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
 	return 0;
 }
