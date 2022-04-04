42b429fc7ce998babbda06fbcb5fbc6f1efa8b71
rosemutai <chepngetichrose2030@gmail.com>
2022-04-04 14:18:11 +0000

    print name of file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <main.h>
+/**
+ *  *  main - a program that prints its name, followed by a new line
+ *   */
+int main(int argc, char* argv[])
+{
+	        if (argc  0)
+			                printf("%s\n", argv[0]);
+		        return (0);
+}
+
